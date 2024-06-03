aff589b93a4680bb920341b674f08153cee181ba
Mandeep Singh <mandeept@graycelltech.com>
2024-06-03 16:33:14 +0530

    your message

---
+++ pythonProject/10_Logical Operators AND OR.py
@@ -0,0 +1,22 @@
+# AND condition
+age = 20
+voter_id = True
+
+if age >= 18 and voter_id:
+    print('you can vote!')
+
+else:
+    print('you cannot vote')
+
+
+
+# OR condition
+age = 19
+voter_id = False
+
+if (age >= 18 or voter_id):
+    print('you can vote!')
+
+else:
+    print('you cannot vote')
+
